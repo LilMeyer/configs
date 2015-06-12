a82920f826cb1742a3c8fdd0d2fd7fc5063e914f
LilMeyer <romain.cotte@gmail.com>
2015-06-12 16:55:37 +0200

    add git-flow-completion

---
+++ .bash-scripts/git-release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Inspired by
+# https://github.com/fmahnke/shell-semver/blob/master/increment_version.sh
+
+if [ ! -f package.json ];
+then
+   echo "File package.json does not exist."
+   exit 0
+fi
+
+
+version=$(cat package.json | jsawk "return this.version")
+
+# Build array from version string.
+
+a=( ${version//./ } )
+((a[2]++))
+version="${a[0]}.${a[1]}.${a[2]}"
+
+echo $version
+
+# echo $version
+git flow release start $version
+npm version patch --no-git-tag-version
+git commit -am "release(${version}): bump version"
+# git flow release finish "${version}"
+
